1d1f718d5b1a27f4e8e29e3113251afdf28e4b48
christina.beckford <christina.beckford@usli.com>
2019-04-18 17:49:15 -0400

    Created the class libraries and document their role for the Forums.

---
+++ TrackerLibrary/TournamentModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    class TournamentModel
+    {
+        public string TournamentName { get; set; }
+        public decimal EntryFee { get; set; }
+        public List<TeamModel> EnteredTeams { get; set; }
+        public List<PrizeModel> Prizes { get; set; }
+
+        public List <MatchupModel> {get; set;} 
+    
+
+    }
+}
